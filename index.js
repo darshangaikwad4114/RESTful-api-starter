f37d19495591dadf2df9565287de7ac8a15d54f5
darshan <darshangaikwad4114@gmail.com>
2024-12-30 16:07:28 +0530

    Enhance MongoDB connection with options and error handling

--- index.js
+++ index.js
@@ -13,8 +13,13 @@ const port = 3001;
 app.use(express.json());
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGODB_URL).then(() => {
+mongoose.connect(process.env.MONGODB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(() => {
     console.log("MongoDB is connected!");
+}).catch((error) => {
+    console.error("MongoDB connection error:", error);
 });
 
 // Create an todo item
